246edf25dba78fbc090f93e7c06bbf8f08baf4a5
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-07-07 21:21:41 +0800

    add category input select

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ import Image from 'react-graceful-image'
 import Layout from './Layout'
 import Card from './Card'
 import Container from './Container'
+import InputSelect from './InputSelect'
 
 const GET_FEEDS = gql`
   query {
@@ -17,6 +18,10 @@ const GET_FEEDS = gql`
       author
       description
       thumbnail
+      categories
+    }
+    allCategories {
+      name
     }
   }
 `
@@ -44,21 +49,63 @@ function Post({post}) {
   )
 }
 
+function mapCategoriesIntoOptions(categories) {
+  return categories.map(c => ({
+    value: c.name,
+    label: c.name
+  }))
+}
+
 class Home extends Component {
+  state = {
+    selectedCategory: ''
+  }
+
+  handleCategoryChange = e => {
+    this.setState({selectedCategory: e.target.value})
+  }
+
   render() {
+    const {selectedCategory} = this.state
     return (
       <Layout>
         <section>
-          <Container>
-            <Query query={GET_FEEDS}>
-              {({loading, error, data}) => {
-                if (loading) return 'Loading...'
-                if (error) return 'Something wrong :('
+          <Query query={GET_FEEDS}>
+            {({loading, error, data}) => {
+              if (loading) return 'Loading...'
+              if (error) return 'Something wrong :('
 
-                return data.feed.map(post => <Post post={post} key={post.id} />)
-              }}
-            </Query>
-          </Container>
+              return (
+                <React.Fragment>
+                  <Container>
+                    <section>
+                      <div className="row">
+                        <div className="col" style={{margin: '20px 20px'}}>
+                          <InputSelect
+                            name="category"
+                            defaultOption="Pilih Kategori"
+                            onChange={this.handleCategoryChange}
+                            options={mapCategoriesIntoOptions(
+                              data.allCategories
+                            )}
+                          />
+                        </div>
+                      </div>
+                    </section>
+                  </Container>
+                  <Container>
+                    {data.feed.filter(post => {
+                      if (selectedCategory.length === 0) {
+                        return true
+                      }
+
+                      return post.categories.includes(selectedCategory)
+                    }).map(post => <Post post={post} key={post.id} />)}
+                  </Container>
+                </React.Fragment>
+              )
+            }}
+          </Query>
         </section>
       </Layout>
     )
---
+++ src/InputSelect.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function InputSelect({ name, options, defaultOption, ...restProps }) {
+  return (
+    <select name={name} id={name} {...restProps}>
+      {defaultOption && <option value="">{defaultOption}</option>}
+      {options.map(option => (
+        <option value={option.value} key={option.value}>{option.label}</option>
+      ))}
+    </select>
+  )
+}
+
+export default InputSelect
